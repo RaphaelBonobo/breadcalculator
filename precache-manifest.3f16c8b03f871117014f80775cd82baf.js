21ce6b9ec99e607d985df27ed0d680f82ffbd0d4
Bonobo <raphael.bonn@gmail.com>
2020-08-22 14:34:35 +0200

    Updates

---
+++ precache-manifest.3f16c8b03f871117014f80775cd82baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d993a1e32eef7a63af72b01a6903021",
+    "url": "/breadcalculator/index.html"
+  },
+  {
+    "revision": "477962c50c17208fefab",
+    "url": "/breadcalculator/static/css/main.595c9fd3.chunk.css"
+  },
+  {
+    "revision": "36b9b3a7140468beda32",
+    "url": "/breadcalculator/static/js/2.8628dc85.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/breadcalculator/static/js/2.8628dc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477962c50c17208fefab",
+    "url": "/breadcalculator/static/js/main.782770f7.chunk.js"
+  },
+  {
+    "revision": "00a2fe5a9bb0481576ae",
+    "url": "/breadcalculator/static/js/runtime-main.20ef6f23.js"
+  }
+]);
